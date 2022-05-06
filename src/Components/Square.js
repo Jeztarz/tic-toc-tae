7b54da5be81d446c0ef9aab8ac4914f03ec3a9b1
Jeztar <j.jeztar@gmail.com>
2022-05-06 11:35:25 +0700

    finish version1

---
+++ src/Components/Square.js
@@ -0,0 +1,10 @@
+import "../App.css";
+function Square({ val, chooseSquare }) {
+  return (
+    <div className="square" onClick={chooseSquare}>
+      {val}
+    </div>
+  );
+}
+
+export default Square;
